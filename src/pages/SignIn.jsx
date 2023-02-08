fefa9c6d6277c4f3855262b2251136ef56c2a010
Wael <waelassaf121@gmail.com>
2023-02-08 06:09:28 +0300

    User Sign In Functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
 
@@ -19,13 +20,30 @@ const SignIn = () => {
             [e.target.id]: e.target.value,
         }))
     }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            )
+            if (userCredential.user) {
+                navigate("/")
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <>
             <div className="pageContainer">
                 <header>
                     <p className="pageHeader">Welcome Back!</p>
                 </header>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input
                         type="email"
                         className="emailInput"
