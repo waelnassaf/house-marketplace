566830c22af99afcd94d492ecc03e63748c2d2b8
Wael <waelassaf121@gmail.com>
2023-02-08 05:39:58 +0300

    Register User

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    updateProfile,
+} from "firebase/auth"
+import { db } from "../firebase.config"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
 
@@ -20,13 +26,31 @@ const SignUp = () => {
             [e.target.id]: e.target.value,
         }))
     }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const auth = getAuth()
+            const userCredential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            )
+            const user = userCredential.user
+            updateProfile(auth.currentUser, {
+                displayName: name,
+            })
+            navigate("/")
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <>
             <div className="pageContainer">
                 <header>
                     <p className="pageHeader">Welcome Back!</p>
                 </header>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input
                         type="text"
                         className="nameInput"
