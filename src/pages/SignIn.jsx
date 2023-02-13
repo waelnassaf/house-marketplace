33b32b2cbd9963b948b8965b933d8421aae0e792
Wael <waelassaf121@gmail.com>
2023-02-13 07:19:47 +0300

    Implement Google OAuth.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
 import visibilityIcon from "../assets/svg/visibilityIcon.svg"
+import OAuth from "../components/OAuth"
 
 const SignIn = () => {
     const [showPassword, setShowPassword] = useState(false)
@@ -84,7 +85,7 @@ const SignIn = () => {
                     </div>
                 </form>
 
-                {/*    Google OAuth Comp*/}
+                <OAuth />
 
                 <Link to="/sign-up" className="registerLink">
                     Sign Up Instead
