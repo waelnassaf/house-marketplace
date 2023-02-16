46ce4691c2202cfd94aa518d971b47b061b82007
Wael <waelassaf121@gmail.com>
2023-02-16 04:49:43 +0300

    Implement Leaflet.

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { Link, useNavigate, useParams } from "react-router-dom"
 import { getDoc, doc } from "firebase/firestore"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
 import { db } from "../firebase.config"
 import { getAuth } from "firebase/auth"
 import Spinner from "../components/Spinner"
@@ -88,7 +89,32 @@ const Listing = () => {
                 </ul>
 
                 <p className="listingLocationTitle">Location</p>
-                {/*MAP*/}
+
+                <div className="leafletContainer">
+                    <MapContainer
+                        style={{ height: "100%", width: "100%" }}
+                        center={[
+                            listing.geolocation.lat,
+                            listing.geolocation.lng,
+                        ]}
+                        zoom={67}
+                        scrollWheelZoom={false}
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker
+                            position={[
+                                listing.geolocation.lat,
+                                listing.geolocation.lng,
+                            ]}
+                        >
+                            <Popup>{listing.location}</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
 
                 {auth.currentUser?.uid !== listing.userRef && (
                     <Link
